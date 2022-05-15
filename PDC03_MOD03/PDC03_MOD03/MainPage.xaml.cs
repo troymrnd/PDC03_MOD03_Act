610fe65b4e1bdc63fcc07b5c32d23c025d349587
Troy <Troy@DESKTOP-JLCV8UI>
2022-05-15 23:58:39 +0800

    Add project files.

---
+++ PDC03_MOD03/PDC03_MOD03/MainPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace PDC03_MOD03
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private async void Activity01(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new activity1());
+        }
+        private async void Activity02(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new activity2());
+        }
+        private async void Activity03(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new activity3());
+        }
+        private async void Activity04(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new activity4());
+        }
+        private async void Activity05(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new activity5());
+        }
+        private async void Activity06(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new activity6());
+        }
+
+    }
+}
---
+++ PDC03_MOD03/PDC03_MOD03/activity6.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PDC03_MOD03
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class activity6 : ContentPage
+    {
+        public activity6()
+        {
+            InitializeComponent();
+
+
+            this.BindingContext = this;
+            this.IsBusy = false;
+            this.BtnConnect.Clicked += BtnConnect_Clicked;
+        }
+        void BtnConnect_Clicked(object sender, EventArgs e)
+        {
+            this.IsBusy = true;
+        }
+
+    }
+
+}
